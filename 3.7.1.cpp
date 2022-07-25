5bf70e30408adbbb1eccc9c75b3912d7870155f5
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-07-25 10:38:50 +0500

    Preprocessor directives

---
+++ 3.7.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+#define MODE  1
+
+#ifndef MODE
+#error Директива MODE не найдена!
+#endif
+
+void add() {
+    int a = 0;
+    int b = 0;            
+    cout << "Работаю в боевом режиме" << endl;
+    cout << "Введите число 1: ";
+    cin >> a;
+    cout << "Введите число 2: ";
+    cin >> b;
+    cout << "Результат сложения: " << a + b;
+     
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+#if MODE==0
+    cout << "Работаю в режиме тренировки";
+#elif MODE==1
+    add();
+#else 
+    cout << "Неизвестный режим. Завершение работы";
+#endif
+    return 0;
+}
